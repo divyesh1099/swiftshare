10f9994fc440543c91c58008b420144a1f2d1529
Divyesh Vishwakarma <workdivyeshvishwakarma@gmail.com>
2023-08-29 18:11:33 +0530

    Add Multiple ID Feature

--- base.py
+++ base.py
@@ -1,35 +1,42 @@
 import os
 import smtplib
+import pandas as pd
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 import openpyxl
 import tkinter as tk
 from tkinter import filedialog
+def send_email(subject, body, to_email, attachments):
+    # Email configuration
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 587
+    smtp_username = 'ajit0810'
+    smtp_password = "ekcydglhzixwgxxb"
+    sender_email = 'ajit0810@example.com'
 
-def send_email(sender_email, sender_password, recipient_email, subject, body, attachments):
-    try:
-        server = smtplib.SMTP("smtp.gmail.com", 587)
+    # Create the email
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = ', '.join(to_email)
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    for attachment in attachments:
+        with open(".." + attachment, "rb") as attach_file:
+            part = MIMEApplication(attach_file.read(), Name=os.path.basename(attachment))
+            part['Content-Disposition'] = f'attachment; filename="{os.path.basename(attachment)}"'
+            msg.attach(part)
+
+    # Connect to SMTP server and send email
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
         server.starttls()
-        server.login(sender_email, sender_password)
-        
-        msg = MIMEMultipart()
-        msg['From'] = sender_email
-        msg['To'] = recipient_email
-        msg['Subject'] = subject
-        msg.attach(MIMEText(body, 'plain'))
-        
-        for attachment in attachments:
-            with open(attachment, "rb") as file:
-                part = MIMEApplication(file.read(), Name=os.path.basename(attachment))
-                part['Content-Disposition'] = f'attachment; filename="{os.path.basename(attachment)}"'
-                msg.attach(part)
-        
-        server.sendmail(sender_email, recipient_email, msg.as_string())
-        server.quit()
-        print(f"Email sent to {recipient_email}")
-    except Exception as e:
-        print(f"Error sending email to {recipient_email}: {e}")
+        server.login(smtp_username, smtp_password)
+        try:
+            server.sendmail(sender_email, to_email, msg.as_string())
+        except Exception as e:
+            print("Error Sending Email to " + str(to_email) + ". Error: " + str(e))
 
 def getTargetFolder():
     root = tk.Tk()
@@ -39,26 +46,31 @@ def getTargetFolder():
     
     return folder_path
 
+def get_excel_filename(folder_path):
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.xlsx') or filename.endswith('.xls'):
+            return filename
+    return None
 
 def main():
-    sender_email = "@gmail.com"
-    sender_password = ""
     target_folder = getTargetFolder()
-    excel_file = target_folder + "/RecepientsAttachments.xlsx"
-    attachments_folder = target_folder+"/attachments/"
+    excel_file = get_excel_filename(target_folder)
+    attachments_folder = ".."+target_folder
     
-    wb = openpyxl.load_workbook(excel_file)
-    sheet = wb.active
+    # Read Excel sheet
+    excel_data = pd.read_excel(target_folder + "/" + excel_file)
     
-    for row in sheet.iter_rows(min_row=2, values_only=True):
-        recipient_email = row[0]
-        attachment_names = row[1].split(';') if row[1] else []
-        subject = row[2]
-        body = row[3]
+    for index, row in excel_data.iterrows():
+        to_email = row['EmailAddress'].split(',')
+        subject = row['SubjectLine']
+        body = row['Body']
+        order_refs = row['OrderRef'].split(',')
         
-        attachments = [os.path.join(attachments_folder, attachment_name) for attachment_name in attachment_names[0].split()]
+        attachments = [os.path.join(attachments_folder, ref.strip()) for ref in order_refs]
         
-        send_email(sender_email, sender_password, recipient_email, subject, body, attachments)
+        send_email(subject, body, to_email, attachments)
+        print(f"Email sent to: {', '.join(to_email)}")
+
 
 if __name__ == "__main__":
     main()
