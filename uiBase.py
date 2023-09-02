e38789bf19a3b48404d6c31745bfaaa18631e506
Divyesh Vishwakarma <workdivyeshvishwakarma@gmail.com>
2023-09-02 17:21:11 +0530

    UI Extra Features

--- uiBase.py
+++ uiBase.py
@@ -8,7 +8,6 @@ from email.mime.application import MIMEApplication
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
-from tkinter import *
 import subprocess
 from functools import partial
 
@@ -60,7 +59,7 @@ def convertRTFtoHTML(rtf_text):
     return html_content
 
 
-def send_email(subject, body, to_email, attachments):
+def send_email(subject, body, to_email, attachments, root: tk.Tk):
     # Email configuration
     smtp_server = 'smtp.gmail.com'
     smtp_port = 587
@@ -88,8 +87,10 @@ def send_email(subject, body, to_email, attachments):
         server.login(smtp_username, smtp_password)
         try:
             server.sendmail(sender_email, to_email, msg.as_string())
+            tk.Label(root, text=f"Email sent to {str(to_email)}.").pack()
         except Exception as e:
             print("Error Sending Email to " + str(to_email) + ". Error: " + str(e))
+            tk.Label(root, text=f"Error Sending Email to {str(to_email)}. Error {str(e)}").pack()
 
 def getTargetFolder():
     root = tk.Tk()
@@ -104,32 +105,19 @@ def get_excel_filename(folder_path):
         if filename.endswith('.xlsx') or filename.endswith('.xls'):
             return filename
     return None
-def send_emails(rootWindow:Tk):
-    rootWindow.destroy()
-
     
-
+def send_emails(rootWindow:tk.Tk):
     target_folder = getTargetFolder()
     excel_file = get_excel_filename(target_folder)
     attachments_folder = target_folder
-    
-    def destroy_messagebox(messagebox):
-        messagebox.destroy()
 
     if excel_file is None:
         messagebox.showerror("Error", "No Excel file found in the selected folder.")
-        messagebox.bind("<FocusOut>", destroy_messagebox)
         return
 
     # Loading Screen
-    loadingScreen = Tk()
-    loadingScreen.title="Sending Emails"
-    loadingScreen.geometry("400x200")
-
-    label = tk.Label(loadingScreen, text="Email Sending in Progress")
-    label.pack(padx=20, pady=20)
-
-    loadingScreen.mainloop()
+    rootWindow.title="Sending Emails"
+    tk.Label(rootWindow, text="Sending Emails ...").pack()
 
     # Read Excel sheet
     excel_data = pd.read_excel(os.path.join(target_folder, excel_file))
@@ -143,12 +131,13 @@ def send_emails(rootWindow:Tk):
         attachments = [os.path.join(attachments_folder, ref.strip()) for ref in order_refs]
         
         try:
-            send_email(subject, body, to_email, attachments)
-            print(f"Email sent to: {', '.join(to_email)}")
-        except Exception as e:
-            print(f"Error sending email to {', '.join(to_email)}: {e}")
+            send_email(subject, body, to_email, attachments, rootWindow)
+            tk.Label(text=f"Email sent to: {', '.join(to_email)}").pack()
 
-    messagebox.showinfo("Email Sending Complete", "Emails have been sent successfully.")
+        except Exception as e:
+            tk.Label(text=f"Error sending email to {', '.join(to_email)}: {e}").pack()
+
+    tk.Label(text="Email Sending Complete. Emails have been sent successfully.").pack()
 
 def main():
     root = tk.Tk()
@@ -157,8 +146,8 @@ def main():
     frame = tk.Frame(root)
     frame.pack(padx=10, pady=10)
 
-    label = tk.Label(frame, text="Select the target folder containing Excel file and attachments:")
-    label.pack()
+    selectLabel = tk.Label(frame, text="Select the target folder containing Excel file and attachments:")
+    selectLabel.pack()
 
     folder_button = tk.Button(frame, text="Select Folder", command=partial(send_emails, root))
     folder_button.pack()
